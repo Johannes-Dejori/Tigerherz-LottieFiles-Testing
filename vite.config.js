62922697bee5f4d5f57ccf254f6d6d7a96f52c75
Johannes Dejori <johannes.dejori@gmail.com>
2023-11-19 15:45:01 +0100

    Add Lottie Animations incl. Explanations

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "src",
-    assetsInclude: ["./assets/**/*.*"],
+    assetsInclude: [
+        "./assets/**/*.*"
+    ],
     build: {
         emptyOutDir: true,
         outDir: "../dist"
